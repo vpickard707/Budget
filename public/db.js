509a9c3f7a65b388401b47c713060aa0f24e3e1d
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-03-22 00:41:11 -0700

    console logs, testing

--- public/db.js
+++ public/db.js
@@ -14,5 +14,42 @@ request.onsuccess = function (event) {
     checkDB();
   }
 };
+request.onerror = function (event) {
+  console.log("Oops! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+}
+
+function checkDB() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    console.log(getAll.result);
+    if (getAll.result.length > 0) {
+      console.log(getAll.result);
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
 
 window.addEventListener("online", checkDatabase);
