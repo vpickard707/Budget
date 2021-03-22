cf22b2de6227956b74d706eb59a93c671ca1037b
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-03-22 00:39:00 -0700

    adding functions

---
+++ public/db.js
@@ -0,0 +1,18 @@
+let db;
+
+const request = indexedDB.open("budgettrack", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDB();
+  }
+};
+
+window.addEventListener("online", checkDatabase);
